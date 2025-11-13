7668887c86d86cb3465c5c4c7c7ff7ddb2265b36
naresh <naresh.tap@gmail.com>
2025-11-13 18:59:50 +0530

    django tests

---
+++ myproject/myapp/tests/test_contact_data_transformer.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+def multiply(a, b):
+    return a * b
+
+class ContactDataTransformerTest(TestCase):
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
